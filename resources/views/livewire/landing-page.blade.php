7f8704a28fee2a34ddae1ea3312d61f11db32c50
agcodex <agcodex01@gmail.com>
2024-06-17 17:51:29 +0800

    feat(landing-page): Partial change: waiting for categories

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -11,7 +11,7 @@
                     access to resources. It provides automated notifications, data analytics for librarians, and
                     supports sustainability, enhancing the overall library experience.
                 </p>
-                <a href="#"
+                <a href="#books"
                     class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Get started
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
@@ -21,28 +21,62 @@
                             clip-rule="evenodd"></path>
                     </svg>
                 </a>
-                <a href="#"
-                    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Speak to Sales
-                </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
                 <img src="/images/book.svg" alt="mockup" />
             </div>
         </div>
     </section>
-    <section id="pricing" class="bg-white dark:bg-gray-900">
+    <section id="books" class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                    Designed for business teams like yours
+                    Explore books that fits to your needs
                 </h2>
                 <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
-                    We focus on markets where technology, innovation,
-                    and capital can unlock long-term value and drive economic growth.
+                    We show you what most interest you and helps you in getting better.
                 </p>
             </div>
-            {{ $this->booksInfolist }}
+            <div class="relative rounded-xl overflow-auto">
+                <div class="relative">
+                    <h3 class="text-2xl font-bold">Trending</h3>
+                    <!-- Contents -->
+                    <div class="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto py-8">
+                        @foreach ($books as $book)
+                            <div class="snap-center shrink-0">
+                                <img class="shrink-0 w-100 h-40 rounded-lg shadow-xl bg-white"
+                                    src="{{ asset('/storage/' . $book->cover) }}">
+                                <div class="flex py-2">
+                                    <x-filament::button class="w-full">Borrow</x-filament::button>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        <div class="snap-center shrink-0">
+                            <div class="shrink-0 w-4 sm:w-48"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="relative mt-10">
+                    <h3 class="text-2xl font-bold">Most Borrow</h3>
+                    <!-- Contents -->
+                    <div class="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto py-8">
+                        @foreach ($books->take(4) as $book)
+                            <div class="snap-center shrink-0">
+                                <img class="shrink-0 w-100 h-40 rounded-lg shadow-xl bg-white"
+                                    src="{{ asset('/storage/' . $book->cover) }}">
+                                <div class="flex py-2">
+                                    <x-filament::button class="w-full">Borrow</x-filament::button>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        <div class="snap-center shrink-0">
+                            <div class="shrink-0 w-4 sm:w-48"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </div>
