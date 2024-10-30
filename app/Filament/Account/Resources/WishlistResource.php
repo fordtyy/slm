cbb010015fded19601fcbf20d31ebf0357657f38
Jan Tancinco <78480878+xxjantancincoxx@users.noreply.github.com>
2024-10-30 21:21:05 +0800

    allow multiple borrow books from wishlist (#59)
    * allow multiple borrow books from wishlist
    
    * removed unused imports

--- app/Filament/Account/Resources/WishlistResource.php
+++ app/Filament/Account/Resources/WishlistResource.php
@@ -3,15 +3,20 @@
 namespace App\Filament\Account\Resources;
 
 use App\Filament\Account\Resources\WishlistResource\Pages;
+use App\Models\BookBorrow;
 use App\Models\BookUser;
+use App\Models\User;
+use Exception;
 use Filament\Forms\Form;
-use Filament\Navigation\NavigationItem;
+use Filament\Notifications\Actions\Action as NotifActions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Build;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
@@ -56,9 +61,43 @@ class WishlistResource extends Resource
                 Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                BulkAction::make('Borrow Books')
+                ->action(function (Collection $records, array $data) {
+                    try {
+                        $borrowRecord = Auth::user()->borrows()->create(['user_id' => Auth::id()]);
+
+                        $wishlistIds = $records->pluck('id')->toArray();
+                        $data = self::getBookIdsWithBookBorrowId($records, $borrowRecord);
+                    
+                        BookBorrow::insert($data);
+                        BookUser::whereIn('id', $wishlistIds)->delete();
+
+                        Notification::make()
+                            ->title('New Borrow Created!')
+                            ->success()
+                            ->actions([
+                                NotifActions::make('view_student')
+                                    ->hidden(fn() => Auth::user()->type === 'admin')
+                                    ->url(fn() => route('filament.account.resources.borrows.view', $borrowRecord))
+                                    ->label('View Request')
+                            ])
+                            ->sendToDatabase(Auth::user())
+                            ->send();
+                        Notification::make()
+                            ->title('New Borrow Created!')
+                            ->success()
+                            ->actions([
+                                NotifActions::make('view_admin')
+                                    ->label('View Request')
+                                    ->url(fn() => route('filament.admin.resources.borrows.view', $borrowRecord)),
+                            ])
+                            ->sendToDatabase(User::where('type', 'admin')->first());
+                        header("Refresh:0");
+                    } catch (Exception $e) {
+                        echo 'Message: ' .$e->getMessage();
+                    }
+                })
+                ->requiresConfirmation(),
             ])
             ->paginated(false);
     }
@@ -80,5 +119,20 @@ class WishlistResource extends Resource
     public static function shouldRegisterNavigation(): bool
     {
       return !in_array(Request::route()->getName(), ['filament.account.pages.category-preferred', 'filament.account.pages.author-preferred']);
+    }
+
+    public static function getBookIdsWithBookBorrowId(Collection $records, $borrowRecord): array 
+    {
+        $data = $records->map(function ($item) {
+            return [
+                'book_id' => $item->book_id
+            ];
+        })->toArray();
+
+        foreach ($data as $index => $item) {
+            $data[$index]['borrow_id'] = $borrowRecord->id;
+        }
+
+        return $data;
     }
 }
