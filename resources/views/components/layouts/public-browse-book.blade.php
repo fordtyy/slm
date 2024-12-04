71432931d668abe64f2e601cc60ba8ea2443c0c2
fordtyy <johncliffordbenaires293@gmail.com>
2024-12-05 00:57:25 +0800

    blocked and public

--- resources/views/components/layouts/public-browse-book.blade.php
+++ resources/views/components/layouts/public-browse-book.blade.php
@@ -7,6 +7,7 @@
     <meta name="application-name" content="{{ config('app.name') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset("/images/favicon.png")}}">
 
     <title>{{ config('app.name') }}</title>
 
